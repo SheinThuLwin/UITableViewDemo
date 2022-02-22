220c7d3cc068680f7fe92f05694a2a408d9622b1
Shein <sheinthulwin@gmail.com>
2022-02-22 13:05:04 +0630

    Initial Commit

---
+++ UITableViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewDemo
+//
+//  Created by ut-i02 on 22/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
